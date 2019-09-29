7af77db7c6cca71e15d0bf768777207f251d056e
luccasr73 <luccasrobert@hotmail.com>
2019-09-29 13:33:12 -0300

    Added another example

--- examples/index.js
+++ examples/index.js
@@ -1,8 +1,16 @@
 /* eslint-disable no-console */
 const lottery = require('./../index')
+
+lottery.resultByNumber('megasena')
+  .then((result) => {
+    console.log('megasena result: ', result)
+  }).catch((e) => {
+    console.log(e)
+  })
+
 lottery.resultByNumber('quina')
   .then((result) => {
-    console.log(result)
+    console.log('quina result: ', result)
   }).catch((e) => {
     console.log(e)
   })
